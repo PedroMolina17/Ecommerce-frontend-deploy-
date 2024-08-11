e6727f61db80b80b145f6a0e26814f3230cfab53
pedromolina17 <pedro.molinanoa@gmail.com>
2024-08-11 00:48:01 -0500

    Fix Product

--- src/Components/Admin/Products/Product.tsx
+++ src/Components/Admin/Products/Product.tsx
@@ -1,24 +1,26 @@
+// Product.tsx
 import useProductStore from "./store/ProductStore";
 import CreateProduct from "./CreateProduct";
 import ProductsTable from "./ProductsTable";
 import UpdateProduct from "./UpdateProduct";
+
 const Product = () => {
   const { operation, productId } = useProductStore();
 
   return (
     <div>
       {operation === "Create" ? (
-        <div>
-          <CreateProduct />
-        </div>
+        <CreateProduct />
       ) : operation === "ViewProduct" ? (
         <ProductsTable />
       ) : operation === "Edit" ? (
-        <UpdateProduct productId={productId} />
+        productId !== null ? (
+          <UpdateProduct productId={productId} />
+        ) : (
+          <div>No product ID available for editing</div>
+        )
       ) : (
-        <div>
-          <CreateProduct />
-        </div>
+        <CreateProduct />
       )}
     </div>
   );
